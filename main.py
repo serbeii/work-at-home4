0211706ba71428e4e5ffc8c417e6be9b3a1c4329
Bequery10 <Berkan.gokgoz@tedu.edu.tr>
2025-03-13 18:38:56 +0300

    add comments

--- main.py
+++ main.py
@@ -17,31 +17,33 @@ client = genai.Client(api_key=API_KEY)
 model_name = "gemini-2.0-flash-lite"
 chatbot = Chatbot(client, model_name)
 
+
 def test(message, history):
     global chatbot
-    result = chatbot.chat_prompt(message) 
+    result = chatbot.chat_prompt(message)
     return result
 
 
 if __name__ == "__main__":
     # start initialization
-    #load_dotenv()
-    #API_KEY = os.getenv("GOOGLE_API_KEY")
-    #client = genai.Client(api_key=API_KEY)
+    # load_dotenv()
+    # API_KEY = os.getenv("GOOGLE_API_KEY")
+    # client = genai.Client(api_key=API_KEY)
 
     model_name = "gemini-2.0-flash-lite"
 
-    models_list = client.models.list(config={"page_size": 5})
+    models_list = client.models.list(config={"page_size": 5})  # get the list of models
     model = None
 
-    for model1 in models_list:
+    for model1 in models_list:  # find the model: gemini-2.0-flash-lite
         if model1.name == "models/" + model_name:
             model = model1
             break
 
     chatbot = Chatbot(client, model)
     # end initialization
-    demo = gr.ChatInterface(test, type="messages",
-                            title="work@home4", description="Lorem Ipsum Dolor")
-    demo.launch()
+    demo = gr.ChatInterface(
+        test, type="messages", title="work@home4", description="Lorem Ipsum Dolor"
+    )  # create a chat interface
+    demo.launch()  # launch the chat interface
     # start_app(chatbot)
